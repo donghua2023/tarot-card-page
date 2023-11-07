3f8651ddc19d65d803f16c12e9e1efa3e4029256
浅陌 <1687516725@qq.com>
2023-11-07 17:57:13 +0800

    chore： vite配置更新

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+
+/** 路径查找 */
+const pathResolve = (dir) => {
+  return resolve(__dirname, ".", dir);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    },
+  },
+  build: {
+    // https://cn.vitejs.dev/guide/build.html#browser-compatibility
+    target: "es2015",
+    sourcemap: false,
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      input: {
+        index: pathResolve("index.html")
+      },
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]"
+      }
+    }
+  },
   plugins: [vue()],
 })
