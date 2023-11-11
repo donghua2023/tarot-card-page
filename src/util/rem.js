4f05549af1a64d41f0a5a815e7827c3e2a0f17be
浅陌 <1687516725@qq.com>
2023-11-11 17:19:53 +0800

    fix： 修复rem配置

--- src/util/rem.js
+++ src/util/rem.js
@@ -1,25 +1,31 @@
+// 设计稿基准宽度
+const baseWidth = 1920;
+const baseHeight = 1080;
+
 // 基准大小
 const baseSize = 192; // 以设计稿宽度的1/10（基础分辨率 1920 * 1080）
+
 // 设置 rem 函数
 function setRem() {
-  let scale = document.body.clientWidth / 1920; // 当前页面宽度相对于 1920 宽的缩放比例，可根据自己需要修改
-  console.log(scale, '----')
-  // 兼容横竖屏
-  if (document.body.clientWidth < document.body.clientHeight) {
-    //   竖屏
-    scale = document.body.clientWidth / 1080;
+  let scale; 
+  // 判断横竖屏状态
+  if (window.innerWidth < window.innerHeight) {
+    // 竖屏
+    scale = window.innerWidth / baseHeight;
+  } else {
+    // 横屏
+    scale = window.innerWidth / baseWidth;
   }
-  if (document.body.clientWidth > document.body.clientHeight) {
-    //   横屏
-    scale = document.body.clientWidth / 1920;
-  }
+  // 限制最大缩放比例为2倍
+  scale = Math.min(scale, 2); 
   // 设置页面根节点字体大小
-  document.documentElement.style.fontSize = `${baseSize * Math.min(scale, 2)}px`;
+  document.documentElement.style.fontSize = `${baseSize * scale}px`;
 }
 
 // 初始化
 setRem();
-// 改变窗口时重新设置rem
+
+// 监听窗口大小变化，重新设置 rem
 window.onresize = () => {
   setRem();
 };
