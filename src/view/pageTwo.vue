7ff8bfcc069cade042f83ec62b1ee33b9fa30add
浅陌 <1687516725@qq.com>
2023-11-18 22:43:26 +0800

    fix

--- src/view/pageTwo.vue
+++ src/view/pageTwo.vue
@@ -1,44 +1,47 @@
 <template>
   <div class="two" ref="containerRef">
     <navBar :isShow="false"></navBar>
-    <img src="../assets/img/two/boxBg.png" alt="背景" class="img">
+    <img src="../assets/img/two/boxBg.png" alt="背景" class="img" />
   </div>
 </template>
 
 <script setup>
-import navBar from '@/components/navBar/navBar.vue'
-import * as THREE from 'three'
-import CLOUD from 'vanta/dist/vanta.clouds.min'
-import {ref, onMounted, onBeforeUnmount} from 'vue'
-const containerRef = ref()
+import navBar from "@/components/navBar/navBar.vue";
+import * as THREE from "three";
+import FOG from "vanta/dist/vanta.fog.min";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+const containerRef = ref();
 
-let vantaEffect=ref(null);
+let vantaEffect = ref(null);
 let vantaEffectStyle = ref(null);
 onMounted(() => {
-  vantaEffect.value = CLOUD({
+  vantaEffect.value = FOG({
     el: containerRef.value,
-		THREE: THREE,
+    THREE: THREE,
   });
-  vantaEffectStyle.value =  CLOUD({
-		el: containerRef.value,
-		/*以下为样式配置*/
-		mouseControls: true,
-		touchControls: true,
-		gyroControls: false,
-		minHeight: 200.0,
-		minWidth: 200.0,
-		scale: 1.0,
-    highlightColor: 0x64647a,
-    midtoneColor: 0x310606,
-    lowlightColor: 0x25547,
-		baseColor: 0x251c1c
-	});
-})
-onBeforeUnmount(()=>{
-  if(vantaEffect.value){
-    vantaEffect.value.destroy()
+  vantaEffectStyle.value = FOG({
+    el: containerRef.value,
+    /*以下为样式配置*/
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    highlightColor: 0x393535,
+    midtoneColor: 0x391612,
+    lowlightColor: 0x3262be,
+    baseColor: 0x1e0a0a,
+    speed: 1,
+    blurFactor: 0.6,
+    zoom: 1,
+  });
+});
+onBeforeUnmount(() => {
+  if (vantaEffect.value) {
+    vantaEffect.value.destroy();
   }
-})
+});
 </script>
 
 <style src="@/assets/css/two.css"></style>
