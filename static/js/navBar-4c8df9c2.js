00d6b24745649d388cfad3e265ae92b035b8a255
浅陌 <1687516725@qq.com>
2023-11-18 21:39:47 +0800

    fix: 打包

--- static/js/navBar-4c8df9c2.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./index-6644b875.js";const s=t("navBar",{state:()=>({isActive:"/pageOne",num:"1"}),actions:{reloadIsActive(e){this.isActive=e},numChange(e){this.num=e}}});export{s as u};
