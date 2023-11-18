13fb52201b6a7835b6d3aa3e9e16bc7535c89ea3
浅陌 <1687516725@qq.com>
2023-11-18 19:27:31 +0800

    style: 改一下路由动画

--- static/js/navBar-408a2bf7.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./index-e5d92a96.js";const s=t("navBar",{state:()=>({isActive:"/pageOne",num:"1"}),actions:{reloadIsActive(e){this.isActive=e},numChange(e){this.num=e}}});export{s as u};
