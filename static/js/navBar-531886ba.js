32b400f8d12b43a30770b2175a109205eb716eb5
浅陌 <1687516725@qq.com>
2023-11-18 19:38:20 +0800

    feat: info路由取消

---
+++ static/js/navBar-531886ba.js
@@ -0,0 +1,1 @@
+import{k as t}from"./index-c012e193.js";const s=t("navBar",{state:()=>({isActive:"/pageOne",num:"1"}),actions:{reloadIsActive(e){this.isActive=e},numChange(e){this.num=e}}});export{s as u};
