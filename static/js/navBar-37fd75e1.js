f49dc43de96aedef605992abe47d0af31a496f8d
浅陌 <1687516725@qq.com>
2023-11-18 14:24:45 +0800

    feat: 完成

--- static/js/navBar-37fd75e1.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-8b296e43.js";const s=t("navBar",{state:()=>({isActive:"/pageOne"}),actions:{reloadIsActive(e){this.isActive=e}}});export{s as u};
---
+++ static/js/navBar-408a2bf7.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index-e5d92a96.js";const s=t("navBar",{state:()=>({isActive:"/pageOne",num:"1"}),actions:{reloadIsActive(e){this.isActive=e},numChange(e){this.num=e}}});export{s as u};
