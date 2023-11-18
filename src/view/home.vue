151b6a6c41a9bc0093560d7e06674dbc11a1c1ff
浅陌 <1687516725@qq.com>
2023-11-18 21:58:24 +0800

    fix

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,57 +1,69 @@
 <template>
   <div class="container">
-    <img src="@/assets/img/hudie.png" alt="蝴蝶" class="img" @mouseover="flapWings" @mouseout="stopFlapping" @click="imgClick" ref="butterflyRef">
-    <img :src="thatImg" alt="文字" class="textImg" @mouseenter="changeImage" @mouseleave="resetImage">
+    <img
+      src="@/assets/img/hudie.png"
+      alt="蝴蝶"
+      class="img"
+      @mouseover="flapWings"
+      @mouseout="stopFlapping"
+      @click="imgClick"
+      ref="butterflyRef"
+    />
+    <img
+      :src="thatImg"
+      alt="文字"
+      class="textImg"
+      @mouseenter="changeImage"
+      @mouseleave="resetImage"
+    />
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from "vue";
 const butterflyRef = ref();
 
-import { useRouter } from 'vue-router';
-const thatImg = ref('')
-thatImg.value = getImageUrl('../assets/img/text.png')
+import { useRouter } from "vue-router";
+const thatImg = ref("");
+thatImg.value = getImageUrl("text");
 const flapWings = () => {
-  const butterfly = document.querySelector('.img');
-  butterfly.style.transform = 'scaleX(-1)';
-  butterfly.style.transform = 'scaleX(0.4)';
-  thatImg.value = getImageUrl('../assets/img/textLight.png')
-}
-const stopFlapping = ()=> {
-  const butterfly = document.querySelector('.img');
-  butterfly.style.transform = 'scaleX(1)';
-  thatImg.value = getImageUrl('../assets/img/text.png')
-}
+  const butterfly = document.querySelector(".img");
+  butterfly.style.transform = "scaleX(-1)";
+  butterfly.style.transform = "scaleX(0.4)";
+  thatImg.value = getImageUrl("textLight");
+};
+const stopFlapping = () => {
+  const butterfly = document.querySelector(".img");
+  butterfly.style.transform = "scaleX(1)";
+  thatImg.value = getImageUrl("text");
+};
 
-const router = useRouter()
+const router = useRouter();
 const imgClick = () => {
   const butterfly = butterflyRef.value;
   butterfly.style.transition = "top 1.5s";
   butterfly.style.top = "-150px";
-  router.push('/index')
-}
+  router.push("/index");
+};
 function getImageUrl(name) {
-  return new URL(`${name}`, import.meta.url).href;
+  return new URL(`../assets/img/${name}.png`, import.meta.url).href;
 }
 
-const changeImage = ()=> {
-  thatImg.value = getImageUrl('../assets/img/textLight.png')
-}
-const resetImage = ()=> {
-  thatImg.value = getImageUrl('../assets/img/text.png')
-}
-
-
+const changeImage = () => {
+  thatImg.value = getImageUrl("textLight");
+};
+const resetImage = () => {
+  thatImg.value = getImageUrl("text");
+};
 </script>
 
 <style scoped>
 .container {
   width: 1920px;
   height: 100vh;
-  background: url('@/assets/img/one/bigBg.png') center no-repeat;
+  background: url("@/assets/img/one/bigBg.png") center no-repeat;
   background-size: 100% 100vh;
-  background-color: #000000!important;
+  background-color: #000000 !important;
   border-radius: 0px 0px 0px 0px;
   opacity: 1;
   display: flex;
