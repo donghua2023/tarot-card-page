677319a21077a6d5e176e416f50eef6bc5aa3eb3
浅陌 <1687516725@qq.com>
2023-11-18 21:46:02 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -24,12 +24,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: pathResolve("index.html")
-      },
-      // 静态资源分类打包
-      output: {
-        chunkFileNames: "static/js/[name]-[hash].js",
-        entryFileNames: "static/js/[name]-[hash].js",
-        assetFileNames: "static/[ext]/[name]-[hash].[ext]"
       }
     }
   },
